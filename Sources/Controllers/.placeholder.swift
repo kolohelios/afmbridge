5aa35431f406546a6b2e935e2b2861e92cb981b5
Jon Edwards <jkedwards@me.com>
2026-02-27 11:36:22 -0800

    chore: remove Controllers and Middleware placeholder files

--- Sources/Controllers/.placeholder.swift
+++
@@ -1,2 +0,0 @@
-// Placeholder file for Controllers target
-// TODO: Implement actual controllers in future phases
--- Sources/Middleware/.placeholder.swift
+++
@@ -1,2 +0,0 @@
-// Placeholder file for Middleware target
-// TODO: Implement actual middleware in future phases
